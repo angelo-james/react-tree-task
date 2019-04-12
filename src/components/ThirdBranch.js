ec3cac867b839a545973b96da171eaa5203bc842
AJ Arriola <angelojames.arriola@gmail.com>
2019-04-12 09:42:59 -0700

    initial boiler plater for ThirdBranch component

---
+++ src/components/ThirdBranch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ThirdBranch extends Component {
+  render() {
+    return (
+      <p>
+        ThirdBranch Component
+      </p>
+    );
+  }
+}
+
+export default ThirdBranch;
