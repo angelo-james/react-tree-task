207b53e48c4803e66a58b8de1d094771cf209e69
AJ Arriola <angelojames.arriola@gmail.com>
2019-04-12 09:33:43 -0700

    mapped through children props and passed to SecondBranch component as props

--- src/components/FirstBranch.js
+++ src/components/FirstBranch.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import SecondBranch from './SecondBranch';
 
 class FirstBranch extends Component {
   render() {
     let { name, children } = this.props;
 
     return (
-      <h1>
-        {name}
-      </h1>
+      <div>
+        <h1>
+          {name}
+        </h1>
+        {children.map(node => (
+          <SecondBranch 
+            name={node.name}
+            children={node.children}
+          />
+        ))}
+      </div>
     );
   }
 }
