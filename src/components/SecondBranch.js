d0d7dc4571781979c657d6ce0287b74015c09ecc
AJ Arriola <angelojames.arriola@gmail.com>
2019-04-12 09:34:44 -0700

    initial boiler plate for SecondBranch component

---
+++ src/components/SecondBranch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SecondBranch extends Component {
+  render() {
+    return (
+      <h1>
+        SecondBranch Component
+      </h1>
+    );
+  }
+}
+
+export default SecondBranch;
