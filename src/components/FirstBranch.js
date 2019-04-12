aa23e72f48f2aa488d83bd631dc71c0f5e7e51e3
AJ Arriola <angelojames.arriola@gmail.com>
2019-04-12 09:20:47 -0700

    created 'component' directory within src folder for all components and initial boiler plate for FirstBranch component

---
+++ src/components/FirstBranch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FirstBranch extends Component {
+  render() {
+    return (
+      <h1>
+        FirstBranch Component
+      </h1>
+    );
+  }
+}
+
+export default FirstBranch;
